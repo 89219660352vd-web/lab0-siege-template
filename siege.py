50dd7c8b1d87c862e85a03d2482a29f2807bb32b
89219660352vd-web <89219660352vd@gmail.com>
2026-09-23 22:12:52 +0300

    Update siege.py

--- siege.py
+++ siege.py
@@ -10,6 +10,42 @@ def main(path: str) -> None:
     # TODO: прочитать лог, посчитать урон, напечатать строки вида
     # "Игрок <Имя> из гильдии <Тег> нанес <Урон> по воротам."
     pass
+    raw_log=open(path)
+    lines=[line for line in raw_log.readlines()[2:]]
+
+    def g(line):
+        if line == '':
+            return None
+        line = line.replace(',', '.')
+        line = line.replace(' ', '')
+        line = line.strip()
+        et = line.find(']')
+        guild = line[1:et]
+        ost = line[et + 1:]
+        parts = ost.split('|')
+        if len(parts) != 4:
+            return None
+        name = parts[0].strip()
+        damage = float(parts[1].strip())
+        weapon = parts[2].strip()
+        buffs = parts[3].strip()
+        if buffs == '' or buffs == 'N/A':
+            k = 0
+        else:
+            k = float(buffs)
+        if weapon == 'Active':
+            state_multiplier = 1.5
+        elif weapon == 'Broken':
+            state_multiplier = 0.5
+        else:
+            state_multiplier = 1.0
+        totdam = damage * state_multiplier * (1 + 0.15 * k)
+        totdam = round(damage, 2)
+        result = "Игрок " + name + " из гильдии " + guild + " нанес " + str(damage) + " по воротам."
+        return result
+
+    for i in lines:
+        print(g(i))
 
 
 if __name__ == "__main__":
